ccc4a25f90502e2c8a1ea4455d5061ad1339fee9
Archie <archie@fundyourselfnow.com>
2018-09-25 14:46:23 +0800

    modified for more generic support

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,30 @@
+static mut COUNT: i64 = 0;
 
+#[no_mangle]
+pub unsafe extern fn countup() -> i64 {
+    COUNT += 1;
+    COUNT
+}
+
+#[no_mangle]
+pub unsafe extern fn reset() {
+    COUNT = 0
+}
 
 #[cfg(target_os="android")]
 pub mod android {
     extern crate jni;
     use self::jni::sys::{jlong};
-
-    static mut COUNT: jlong = 0;
+    use countup;
+    use reset;
 
     #[no_mangle]
     pub unsafe extern fn Java_rakuten_test_technical_countup_Counter_countup() -> jlong {
-        COUNT += 1;
-        COUNT
+       countup()
     }
 
     #[no_mangle]
     pub unsafe extern fn Java_rakuten_test_technical_countup_Counter_reset() {
-        COUNT = 0;
+        reset()
     }
 }
